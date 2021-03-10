ba6bc21480fb055664832095521bd6e1d84569d8
Fernando <fernandovalentedev@gmail.com>
2021-03-10 10:41:25 -0300

    update packge, create mid and post add prod rest to test

--- index.js
+++ index.js
@@ -4,8 +4,29 @@ import Products from "./products.js";
 const app = Express();
 const port = 3000;
 
+let mid = (req, res, next) => {
+    console.log(req.query);
+    console.log(req.params);
+    next();
+};
+
+app.use(Express.json());
+app.use(Express.urlencoded({extended: true}));
+
+// Controllers
 app.get("/", (req, res) => {
     res.json(Products);
+});
+
+app.get("/products/:id", (req, res) => {
+    res.json(Products.find( (product) => {
+        return +req.params.id === product.id;
+    }));
+});
+
+app.post("/products/add", mid, (req, res) => {
+    console.log(req.body);
+    res.sendStatus(200);
 });
 
 app.listen(port, () => console.log('Listening on port ' + port));
