3278e8bebbcf431b4b299e29d5676f5f2f257be6
Fernando <fernandovalentedev@gmail.com>
2021-03-10 17:20:10 -0300

    chore: update settings and set up consign

---
+++ controllers/product.js
@@ -0,0 +1,21 @@
+const Products = require('../listproducts.js');
+
+module.exports = app => {
+    const products = Products();
+
+    app.get("/products", (req, res) => {
+        res.json(products);
+    });
+    
+    app.get("/products/:id", (req, res) => {
+        res.json(products.find( (product) => {
+            return +req.params.id === product.id;
+        }));
+    });
+    
+    // // app.post("/products/add", mid, (req, res) => {
+    app.post("/products/add", (req, res) => {
+        console.log(req.body);
+        res.sendStatus(200);
+    });
+};
---
+++ listproducts.js
@@ -0,0 +1,20 @@
+module.exports = product => {
+    return [
+        {
+            "id": 1,
+            "name": "Aenean viverra rhoncus"
+        },
+        {
+            "id": 2,
+            "name": "Maecenas tempus tellus"
+        },
+        {
+            "id": 3,
+            "name": "Quisque id odio"
+        },
+        {
+            "id": 4,
+            "name": "Phasellus volutpat metus"
+        }
+    ]
+}
